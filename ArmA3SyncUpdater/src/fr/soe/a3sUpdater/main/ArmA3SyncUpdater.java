86e7b435ab56dfdf588ab753b44c0643431c8945
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2013-08-04 17:47:56 +0000

    Linux: fixe LAF.
    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/trunk@171 73b07b11-db6f-9345-a1c1-cd074a5028ff

--- ArmA3SyncUpdater/src/fr/soe/a3sUpdater/main/ArmA3SyncUpdater.java
+++ ArmA3SyncUpdater/src/fr/soe/a3sUpdater/main/ArmA3SyncUpdater.java
@@ -1,5 +1,10 @@
 package fr.soe.a3sUpdater.main;
 
+import java.awt.Font;
+
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
+
 import fr.soe.a3sUpdater.ui.Facade;
 import fr.soe.a3sUpdater.ui.Updater;
 
@@ -13,13 +18,21 @@ public class ArmA3SyncUpdater {
 			if (osName.contains("Windows")) {
 				javax.swing.UIManager
 						.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-			} else if (osName.contains("Linux")) {
-				javax.swing.UIManager
-						.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+			} else {
+				Font fontButton = UIManager.getFont("Button.font");
+				Font fontButtonA3S = new Font(fontButton.getName(),Font.PLAIN,11);
+				javax.swing.UIManager.put("Button.font", new FontUIResource(fontButtonA3S));
+				
+				Font fontLabel = UIManager.getFont("Label.font");
+				Font fontLabelA3S = new Font(fontLabel.getName(),Font.PLAIN,11);
+				UIManager.put("Label.font", new FontUIResource(fontLabelA3S));
+				
+				Font fontTextField = UIManager.getFont("TextField.font");
+				Font fontTextFieldA3S = new Font(fontTextField.getName(),fontTextField.getStyle(),11);
+				UIManager.put("TextField", new FontUIResource(fontTextFieldA3S));
 			}
 		} catch (Exception e) {
 		}
-		
 		
 		String path = "";
 		if (args.length != 0) {
