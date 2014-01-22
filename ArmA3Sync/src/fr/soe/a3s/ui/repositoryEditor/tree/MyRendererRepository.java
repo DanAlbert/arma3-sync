1acdf5bd74f17781fd2e7e1be44f9305f328c12e
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2014-01-22 17:08:28 +0000

    Modifié: Download Panel Tree: pas d'icône pour les dossiers de type Addons.
    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/trunk@822 73b07b11-db6f-9345-a1c1-cd074a5028ff

--- ArmA3Sync/src/fr/soe/a3s/ui/repositoryEditor/tree/MyRendererRepository.java
+++ ArmA3Sync/src/fr/soe/a3s/ui/repositoryEditor/tree/MyRendererRepository.java
@@ -2,7 +2,6 @@ package fr.soe.a3s.ui.repositoryEditor.tree;
 
 import java.awt.Component;
 
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
@@ -12,33 +11,35 @@ import fr.soe.a3s.dto.sync.SyncTreeDirectoryDTO;
 import fr.soe.a3s.dto.sync.SyncTreeNodeDTO;
 import fr.soe.a3s.ui.UIConstants;
 
-public class MyRendererRepository extends DefaultTreeCellRenderer implements
-		UIConstants {
+public class MyRendererRepository extends DefaultTreeCellRenderer implements UIConstants {
 
-	public Component getTreeCellRendererComponent(JTree tree, Object value,
-			boolean sel, boolean expanded, boolean isLeaf, int row,
-			boolean hasFocus) {
-		super.getTreeCellRendererComponent(tree, value, sel, expanded, isLeaf,
-				row, hasFocus);
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel,
+            boolean expanded, boolean isLeaf, int row, boolean hasFocus) {
+        super.getTreeCellRendererComponent(tree, value, sel, expanded, isLeaf, row, hasFocus);
 
-		TreePath path = tree.getPathForRow(row);
-		if (path != null) {
-			SyncTreeNodeDTO syncTreeNodeDTO = (SyncTreeNodeDTO) value;
-			setIcon(syncTreeNodeDTO);
-		}
-		return this;
-	}
+        TreePath path = tree.getPathForRow(row);
+        if (path != null) {
+            SyncTreeNodeDTO syncTreeNodeDTO = (SyncTreeNodeDTO) value;
+            setIcon(syncTreeNodeDTO);
+        }
+        return this;
+    }
 
-	private void setIcon(SyncTreeNodeDTO syncTreeNodeDTO) {
-		if (!syncTreeNodeDTO.isLeaf()) {
-			SyncTreeDirectoryDTO syncTreeDirectoryDTO = (SyncTreeDirectoryDTO) syncTreeNodeDTO;
+    private void setIcon(SyncTreeNodeDTO syncTreeNodeDTO) {
+        if (!syncTreeNodeDTO.isLeaf()) {
+            SyncTreeDirectoryDTO syncTreeDirectoryDTO = (SyncTreeDirectoryDTO) syncTreeNodeDTO;
 
-			for (SyncTreeNodeDTO n : syncTreeDirectoryDTO.getList()) {
-				if (n.isUpdated() || n.isDeleted()) {
-					setIcon(new ImageIcon(EXCLAMATION));
-				}
-				setIcon(n);
-			}
-		}
-	}
+            if (syncTreeDirectoryDTO.isMarkAsAddon()) {
+                setIcon(new ImageIcon());
+            }
+
+            for (SyncTreeNodeDTO n : syncTreeDirectoryDTO.getList()) {
+                if (n.isUpdated() || n.isDeleted()) {
+                    setIcon(new ImageIcon(EXCLAMATION));
+                    break;
+                }
+            }
+        }
+    }
 }
