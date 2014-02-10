1c1ecb654fbae82b28fca8c3bf670f45a4c6c1e6
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2014-02-10 23:10:10 +0000

    Fixé: Minimaization Enum ajout de Close.
    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/trunk@870 73b07b11-db6f-9345-a1c1-cd074a5028ff

--- ArmA3Sync/src/fr/soe/a3s/constant/MinimizationType.java
+++ ArmA3Sync/src/fr/soe/a3s/constant/MinimizationType.java
@@ -1,25 +1,28 @@
 package fr.soe.a3s.constant;
 
 public enum MinimizationType {
-	NOTHING("Do nothing"),TASK_BAR("Reduce to task bar"),TRAY("Reduce to tray");
-	
+	NOTHING("Do nothing"), TASK_BAR("Reduce to task bar"), TRAY(
+			"Reduce to tray"), CLOSE("Close");
+
 	private String description;
-	
-	private MinimizationType(String description){
+
+	private MinimizationType(String description) {
 		this.description = description;
 	}
-	
-	public String getDescription(){
+
+	public String getDescription() {
 		return this.description;
 	}
-	
-	public static MinimizationType getEnum(String designation){
-		if (designation.equals("Do nothing")){
+
+	public static MinimizationType getEnum(String designation) {
+		if (designation.equals("Do nothing")) {
 			return NOTHING;
-		}else if (designation.equals("Reduce to task bar")){
+		} else if (designation.equals("Reduce to task bar")) {
 			return TASK_BAR;
-		}else {
+		} else if (designation.equals("Reduce to tray")) {
 			return TRAY;
+		} else {
+			return CLOSE;
 		}
 	}
 }
