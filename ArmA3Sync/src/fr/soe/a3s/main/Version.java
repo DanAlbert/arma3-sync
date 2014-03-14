08d1661cae8bac2b0bcdebf4a9e21a6da315ec98
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2014-03-14 13:19:13 +0000

    Refractor.
    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/trunk@940 73b07b11-db6f-9345-a1c1-cd074a5028ff

--- ArmA3Sync/src/fr/soe/a3s/main/Version.java
+++ ArmA3Sync/src/fr/soe/a3s/main/Version.java
@@ -2,29 +2,29 @@ package fr.soe.a3s.main;
 
 public class Version {
 
-    private static final String NAME = "1.2 Update 5";
+	private static final String NAME = "1.2 Update 5";
 
-    private static final int MAJOR = 1;
+	private static final int MAJOR = 1;
 
-    private static final int MINOR = 2;
+	private static final int MINOR = 2;
 
-    private static final int BUILD = 40;
+	private static final int BUILD = 39;
 
-    private static final String YEAR = "2013-2014";
+	private static final String YEAR = "2013-2014";
 
-    public static String getVersion() {
-        return MAJOR + "." + MINOR + "." + BUILD;
-    }
+	public static String getVersion() {
+		return MAJOR + "." + MINOR + "." + BUILD;
+	}
 
-    public static String getName() {
-        return NAME;
-    }
+	public static String getName() {
+		return NAME;
+	}
 
-    public static int getBuild() {
-        return BUILD;
-    }
+	public static int getBuild() {
+		return BUILD;
+	}
 
-    public static String getYear() {
-        return YEAR;
-    }
+	public static String getYear() {
+		return YEAR;
+	}
 }
