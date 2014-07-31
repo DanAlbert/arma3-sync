5eeac9d6c8bb8eec503cdcd89db7604e49519765
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2014-07-31 09:26:44 +0000

    Fixé: Chargement de Pofiles et Repositories.
    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/trunk@1229 73b07b11-db6f-9345-a1c1-cd074a5028ff

--- ArmA3Sync/src/fr/soe/a3s/dao/ProfileDAO.java
+++ ArmA3Sync/src/fr/soe/a3s/dao/ProfileDAO.java
@@ -18,74 +18,74 @@ import fr.soe.a3s.exception.WritingException;
 
 public class ProfileDAO implements DataAccessConstants {
 
-	private static final Map<String, Profile> mapProfiles = new HashMap<String, Profile>();
+    private static final Map<String, Profile> mapProfiles = new HashMap<String, Profile>();
 
-	public Map<String, Profile> getMap() {
-		return mapProfiles;
-	}
+    public Map<String, Profile> getMap() {
+        return mapProfiles;
+    }
 
-	public void readProfiles() throws LoadingException {
-		File directory = new File(PROFILES_FOLDER_PATH);
-		File[] subfiles = directory.listFiles();
-		boolean error = false;
-		if (subfiles != null) {
-			for (File file : subfiles) {
-				try {
-					ObjectInputStream fRo = new ObjectInputStream(
-							new GZIPInputStream(new FileInputStream(file)));
-					Profile profile = (Profile) fRo.readObject();
-					fRo.close();
-					if (profile != null) {
-						mapProfiles.put(profile.getName(), profile);
-					}
-				} catch (Exception e) {
-					error = true;
-					e.printStackTrace();
-				}
-			}
-		}
+    public void readProfiles() throws LoadingException {
+        File directory = new File(PROFILES_FOLDER_PATH);
+        File[] subfiles = directory.listFiles();
+        boolean error = false;
+        if (subfiles != null) {
+            for (File file : subfiles) {
+                if (file.isFile() && file.getName().contains(PROFILE_EXTENSION)) {
+                    try {
+                        ObjectInputStream fRo = new ObjectInputStream(new GZIPInputStream(
+                                new FileInputStream(file)));
+                        Profile profile = (Profile) fRo.readObject();
+                        fRo.close();
+                        if (profile != null) {
+                            mapProfiles.put(profile.getName(), profile);
+                        }
+                    }
+                    catch (Exception e) {
+                        error = true;
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
 
-		if (!mapProfiles.containsKey(DefaultProfileName.DEFAULT
-				.getDescription())) {
-			Profile profile = new Profile("Default");
-			mapProfiles.put(profile.getName(), profile);
-		}
+        if (!mapProfiles.containsKey(DefaultProfileName.DEFAULT.getDescription())) {
+            Profile profile = new Profile("Default");
+            mapProfiles.put(profile.getName(), profile);
+        }
 
-		if (error) {
-			throw new LoadingException();
-		}
-	}
+        if (error) {
+            throw new LoadingException();
+        }
+    }
 
-	public void writeProfiles() throws WritingException {
+    public void writeProfiles() throws WritingException {
 
-		/* Clear profiles folder */
-		File profilesFolder = new File(PROFILES_FOLDER_PATH);
-		FileAccessMethods.deleteDirectory(profilesFolder);
-		profilesFolder.mkdirs();
+        /* Clear profiles folder */
+        File profilesFolder = new File(PROFILES_FOLDER_PATH);
+        FileAccessMethods.deleteDirectory(profilesFolder);
+        profilesFolder.mkdirs();
 
-		boolean error = false;
-		for (Iterator<String> i = mapProfiles.keySet().iterator(); i.hasNext();) {
-			String profileName = i.next();
-			Profile profile = mapProfiles.get(profileName);
-			try {
-				mapProfiles.put(profile.getName(), profile);
-				String path = PROFILES_FOLDER_PATH + "/" + profile.getName()
-						+ PROFILE_EXTENSION;
-				File file = new File(path);
-				ObjectOutputStream fWo = new ObjectOutputStream(
-						new GZIPOutputStream(new FileOutputStream(file)));
-				if (profile != null)
-					fWo.writeObject(profile);
-				fWo.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-				error = true;
-			}
-		}
+        boolean error = false;
+        for (Iterator<String> i = mapProfiles.keySet().iterator(); i.hasNext();) {
+            String profileName = i.next();
+            Profile profile = mapProfiles.get(profileName);
+            try {
+                mapProfiles.put(profile.getName(), profile);
+                String path = PROFILES_FOLDER_PATH + "/" + profile.getName() + PROFILE_EXTENSION;
+                File file = new File(path);
+                ObjectOutputStream fWo = new ObjectOutputStream(new GZIPOutputStream(
+                        new FileOutputStream(file)));
+                if (profile != null) fWo.writeObject(profile);
+                fWo.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                error = true;
+            }
+        }
 
-		if (error) {
-			throw new WritingException(
-					"Failded to write one or more profile(s).");
-		}
-	}
+        if (error) {
+            throw new WritingException("Failded to write one or more profile(s).");
+        }
+    }
 }
--- ArmA3Sync/src/fr/soe/a3s/dao/RepositoryDAO.java
+++ ArmA3Sync/src/fr/soe/a3s/dao/RepositoryDAO.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
@@ -27,170 +26,170 @@ import fr.soe.a3s.exception.WritingException;
 
 public class RepositoryDAO implements DataAccessConstants {
 
-	private static final Map<String, Repository> mapRepositories = new HashMap<String, Repository>();
-	
-	public Map<String, Repository> getMap() {
-		return mapRepositories;
-	}
+    private static final Map<String, Repository> mapRepositories = new HashMap<String, Repository>();
 
-	public void remove(String repositoryName) {
-		File directory = new File(REPOSITORY_FOLDER_PATH);
-		Repository repository = mapRepositories.get(repositoryName);
-		String concatName = repository.getName().replaceAll(" ", "");
-		File[] subfiles = directory.listFiles();
-		if (subfiles!=null){
-			for (File file : subfiles) {
-				if (file.getName().contains(concatName)) {
-					FileAccessMethods.deleteFile(file);
-				}
-			}
-		}
-	}
+    public Map<String, Repository> getMap() {
+        return mapRepositories;
+    }
 
-	public void readAll(Cipher cipher) throws LoadingException {
+    public void remove(String repositoryName) {
+        File directory = new File(REPOSITORY_FOLDER_PATH);
+        Repository repository = mapRepositories.get(repositoryName);
+        String concatName = repository.getName().replaceAll(" ", "");
+        File[] subfiles = directory.listFiles();
+        if (subfiles != null) {
+            for (File file : subfiles) {
+                if (file.getName().contains(concatName)) {
+                    FileAccessMethods.deleteFile(file);
+                }
+            }
+        }
+    }
 
-		File directory = new File(REPOSITORY_FOLDER_PATH);
-		File[] subfiles = directory.listFiles();
-		boolean error = false;
-		mapRepositories.clear();
-		if (subfiles!=null){
-			for (File file : subfiles) {
-				try {
-					ObjectInputStream fRo = new ObjectInputStream(
-							new GZIPInputStream(new FileInputStream(file)));
-					SealedObject sealedObject = (SealedObject) fRo.readObject();
-					Repository repository = (Repository) sealedObject
-							.getObject(cipher);
-					fRo.close();
-					if (repository != null) {
-						mapRepositories.put(repository.getName(), repository);
-					}
-				} catch (Exception e) {
-					error = true;
-					e.printStackTrace();
-				}
-			}
-		}
+    public void readAll(Cipher cipher) throws LoadingException {
 
-		if (error) {
-			throw new LoadingException();
-		}
-	}
-	
-	public void write(Cipher cipher, String repositoryName)
-			throws WritingException {
+        File directory = new File(REPOSITORY_FOLDER_PATH);
+        File[] subfiles = directory.listFiles();
+        boolean error = false;
+        mapRepositories.clear();
+        if (subfiles != null) {
+            for (File file : subfiles) {
+                if (file.isFile() && file.getName().contains(REPOSITORY_EXTENSION)) {
+                    try {
+                        ObjectInputStream fRo = new ObjectInputStream(new GZIPInputStream(
+                                new FileInputStream(file)));
+                        SealedObject sealedObject = (SealedObject) fRo.readObject();
+                        Repository repository = (Repository) sealedObject.getObject(cipher);
+                        fRo.close();
+                        if (repository != null) {
+                            mapRepositories.put(repository.getName(), repository);
+                        }
+                    }
+                    catch (Exception e) {
+                        error = true;
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
 
-		Repository repository = mapRepositories.get(repositoryName);
-		try {
-			if (repository != null) {
-				String concatName = repository.getName().replaceAll(" ", "");
-				SealedObject sealedObject = new SealedObject(repository, cipher);
-				String filePath = REPOSITORY_FOLDER_PATH + "/" + concatName
-						+ REPOSITORY_EXTENSION;
-				ObjectOutputStream fWo = new ObjectOutputStream(
-						new GZIPOutputStream(new FileOutputStream(filePath)));
-				if (sealedObject != null)
-					fWo.writeObject(sealedObject);
-				fWo.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new WritingException();
-		}
-	}
-	
-	public ServerInfo readServerInfo(String repositoryName) throws FileNotFoundException, IOException, ClassNotFoundException{
-		
-		ServerInfo serverInfo = null;
-		Repository repository = mapRepositories.get(repositoryName);
-		if (repository != null) {
-			String path = repository.getPath();
-			String serverInfoPath = path + "/" + SERVERINFO_FILE_PATH;
-			File file = new File(serverInfoPath);
-			if (file.exists()){
-				ObjectInputStream fRo = new ObjectInputStream(new GZIPInputStream(
-						new FileInputStream(file)));
-				serverInfo = (ServerInfo) fRo.readObject();
-				fRo.close();
-			}
-		}
-		return serverInfo;
-	}
-	
-	public Changelogs readChangelogs(String repositoryName)
-			throws FileNotFoundException, IOException, ClassNotFoundException {
+        if (error) {
+            throw new LoadingException();
+        }
+    }
 
-		Changelogs changelogs = null;
-		Repository repository = mapRepositories.get(repositoryName);
-		if (repository != null) {
-			String path = repository.getPath();
-			String changelogsPath = path + "/" + CHANGELOGS_FILE_PATH;
-			File file = new File(changelogsPath);
-			if (file.exists()) {
-				ObjectInputStream fRo = new ObjectInputStream(
-						new GZIPInputStream(new FileInputStream(file)));
-				changelogs = (Changelogs) fRo.readObject();
-				fRo.close();
-			}
-		}
-		return changelogs;
-	}
-	
-	public AutoConfig readAutoConfig(String repositoryName)
-			throws FileNotFoundException, IOException, ClassNotFoundException {
+    public void write(Cipher cipher, String repositoryName) throws WritingException {
 
-		AutoConfig autoconfig = null;
-		Repository repository = mapRepositories.get(repositoryName);
-		if (repository != null) {
-			String path = repository.getPath();
-			String autocOnfigPath = path + "/" + AUTOCONFIG_FILE_PATH;
-			File file = new File(autocOnfigPath);
-			if (file.exists()) {
-				ObjectInputStream fRo = new ObjectInputStream(
-						new GZIPInputStream(new FileInputStream(file)));
-				autoconfig = (AutoConfig) fRo.readObject();
-				fRo.close();
-			}
-		}
-		return autoconfig;
-	}
+        Repository repository = mapRepositories.get(repositoryName);
+        try {
+            if (repository != null) {
+                String concatName = repository.getName().replaceAll(" ", "");
+                SealedObject sealedObject = new SealedObject(repository, cipher);
+                String filePath = REPOSITORY_FOLDER_PATH + "/" + concatName + REPOSITORY_EXTENSION;
+                ObjectOutputStream fWo = new ObjectOutputStream(new GZIPOutputStream(
+                        new FileOutputStream(filePath)));
+                if (sealedObject != null) fWo.writeObject(sealedObject);
+                fWo.close();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new WritingException();
+        }
+    }
 
-	public SyncTreeDirectory readSync(String repositoryName)
-			throws FileNotFoundException, IOException, ClassNotFoundException {
-		
-		SyncTreeDirectory sync = null;
-		Repository repository = mapRepositories.get(repositoryName);
-		if (repository != null) {
-			String path = repository.getPath();
-			String syncPath = path + "/" + SYNC_FILE_PATH;
-			File file = new File(syncPath);
-			if (file.exists()){
-				ObjectInputStream fRo = new ObjectInputStream(new GZIPInputStream(
-						new FileInputStream(file)));
-				sync = (SyncTreeDirectory) fRo.readObject();
-				fRo.close();
-			}
-		}
-		return sync;
-	}
+    public ServerInfo readServerInfo(String repositoryName) throws FileNotFoundException,
+            IOException, ClassNotFoundException {
 
-	public void saveToDiskEvents(Events events,String repositoryPath) throws WritingException {
-		
-		try {
-			File file = new File(repositoryPath);
-			File a3sFolder = new File(repositoryPath + A3S_FOlDER_PATH);
-			a3sFolder.mkdirs();
-			File eventsFile = new File(file.getAbsolutePath()
-					+ EVENTS_FILE_PATH);
-			if (events != null) {
-				ObjectOutputStream fWo = new ObjectOutputStream(
-						new GZIPOutputStream(new FileOutputStream(
-								eventsFile.getAbsolutePath())));
-				fWo.writeObject(events);
-				fWo.close();
-			}
-		} catch (Exception e) {
-			throw new WritingException(e.getMessage());
-		}
-	}
+        ServerInfo serverInfo = null;
+        Repository repository = mapRepositories.get(repositoryName);
+        if (repository != null) {
+            String path = repository.getPath();
+            String serverInfoPath = path + "/" + SERVERINFO_FILE_PATH;
+            File file = new File(serverInfoPath);
+            if (file.exists()) {
+                ObjectInputStream fRo = new ObjectInputStream(new GZIPInputStream(
+                        new FileInputStream(file)));
+                serverInfo = (ServerInfo) fRo.readObject();
+                fRo.close();
+            }
+        }
+        return serverInfo;
+    }
+
+    public Changelogs readChangelogs(String repositoryName) throws FileNotFoundException,
+            IOException, ClassNotFoundException {
+
+        Changelogs changelogs = null;
+        Repository repository = mapRepositories.get(repositoryName);
+        if (repository != null) {
+            String path = repository.getPath();
+            String changelogsPath = path + "/" + CHANGELOGS_FILE_PATH;
+            File file = new File(changelogsPath);
+            if (file.exists()) {
+                ObjectInputStream fRo = new ObjectInputStream(new GZIPInputStream(
+                        new FileInputStream(file)));
+                changelogs = (Changelogs) fRo.readObject();
+                fRo.close();
+            }
+        }
+        return changelogs;
+    }
+
+    public AutoConfig readAutoConfig(String repositoryName) throws FileNotFoundException,
+            IOException, ClassNotFoundException {
+
+        AutoConfig autoconfig = null;
+        Repository repository = mapRepositories.get(repositoryName);
+        if (repository != null) {
+            String path = repository.getPath();
+            String autocOnfigPath = path + "/" + AUTOCONFIG_FILE_PATH;
+            File file = new File(autocOnfigPath);
+            if (file.exists()) {
+                ObjectInputStream fRo = new ObjectInputStream(new GZIPInputStream(
+                        new FileInputStream(file)));
+                autoconfig = (AutoConfig) fRo.readObject();
+                fRo.close();
+            }
+        }
+        return autoconfig;
+    }
+
+    public SyncTreeDirectory readSync(String repositoryName) throws FileNotFoundException,
+            IOException, ClassNotFoundException {
+
+        SyncTreeDirectory sync = null;
+        Repository repository = mapRepositories.get(repositoryName);
+        if (repository != null) {
+            String path = repository.getPath();
+            String syncPath = path + "/" + SYNC_FILE_PATH;
+            File file = new File(syncPath);
+            if (file.exists()) {
+                ObjectInputStream fRo = new ObjectInputStream(new GZIPInputStream(
+                        new FileInputStream(file)));
+                sync = (SyncTreeDirectory) fRo.readObject();
+                fRo.close();
+            }
+        }
+        return sync;
+    }
+
+    public void saveToDiskEvents(Events events, String repositoryPath) throws WritingException {
+
+        try {
+            File file = new File(repositoryPath);
+            File a3sFolder = new File(repositoryPath + A3S_FOlDER_PATH);
+            a3sFolder.mkdirs();
+            File eventsFile = new File(file.getAbsolutePath() + EVENTS_FILE_PATH);
+            if (events != null) {
+                ObjectOutputStream fWo = new ObjectOutputStream(new GZIPOutputStream(
+                        new FileOutputStream(eventsFile.getAbsolutePath())));
+                fWo.writeObject(events);
+                fWo.close();
+            }
+        }
+        catch (Exception e) {
+            throw new WritingException(e.getMessage());
+        }
+    }
 }
