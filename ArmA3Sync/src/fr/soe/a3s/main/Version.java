ce8480e35ad950adadbe02e2476588ffc6d51760
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2015-08-21 15:05:52 +0000

    Refactor.
    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/trunk@1812 73b07b11-db6f-9345-a1c1-cd074a5028ff

--- ArmA3Sync/src/fr/soe/a3s/main/Version.java
+++ ArmA3Sync/src/fr/soe/a3s/main/Version.java
@@ -2,13 +2,13 @@ package fr.soe.a3s.main;
 
 public class Version {
 
-	private static final String NAME = "1.5 Beta 1";
+	private static final String NAME = "1.5";
 
 	private static final int MAJOR = 1;
 
 	private static final int MINOR = 5;
 
-	private static final int BUILD = 64;
+	private static final int BUILD = 66;
 
 	private static final String YEAR = "2013-2015";
 
