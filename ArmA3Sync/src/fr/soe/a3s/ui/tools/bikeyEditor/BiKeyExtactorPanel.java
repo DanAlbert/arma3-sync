32ef90c3459639d5b3e11f7ad8c14befa2c0cb02
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2015-10-16 16:11:52 +0000

    Save.
    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/trunk@1851 73b07b11-db6f-9345-a1c1-cd074a5028ff

--- ArmA3Sync/src/fr/soe/a3s/ui/tools/bikeyEditor/BiKeyExtactorPanel.java
+++ ArmA3Sync/src/fr/soe/a3s/ui/tools/bikeyEditor/BiKeyExtactorPanel.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +26,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 
+import fr.soe.a3s.dao.FileAccessMethods;
 import fr.soe.a3s.ui.Facade;
 import fr.soe.a3s.ui.ImagePanel;
 import fr.soe.a3s.ui.UIConstants;
@@ -225,8 +227,30 @@ public class BiKeyExtactorPanel extends JDialog implements UIConstants {
 			if (!sourceDirectory.exists()) {
 
 			} else {
-				extractedFiles = new ArrayList<File>();
-				extractBikeyFiles(sourceDirectory);
+				String targetDirectoryPath = textFieldTargetDirectory.getText();
+				if (targetDirectoryPath.isEmpty()) {
+				} else {
+					File targetDiectory = new File(targetDirectoryPath);
+					if (!targetDiectory.exists()) {
+
+					} else {
+						// Check write permissions
+
+						extractedFiles = new ArrayList<File>();
+						extractBikeyFiles(sourceDirectory);
+						if (extractedFiles.isEmpty()) {
+						} else {
+							try {
+								for (File file : extractedFiles) {
+									FileAccessMethods.copyFile(file,
+											targetDiectory);
+								}
+							} catch (IOException e) {
+								//
+							}
+						}
+					}
+				}
 			}
 		}
 	}
