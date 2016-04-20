88f69af4fe2a036805eb6b799f83b6a5b23d6d17
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2016-04-20 12:09:23 +0000

    Refactor.
    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/trunk@2216 73b07b11-db6f-9345-a1c1-cd074a5028ff

--- ArmA3Sync/src/fr/soe/a3s/service/RepositoryService.java
+++ ArmA3Sync/src/fr/soe/a3s/service/RepositoryService.java
@@ -455,7 +455,7 @@ public class RepositoryService extends ObjectDTOtransformer implements
 
 				boolean contains = false;
 				for (String stg : hiddenFolderPaths) {
-					stg = backlashReplace(stg);
+					stg = backslashReplace(stg);
 					if (relativePath.length() >= stg.length()) {
 						String p = relativePath.substring(0, stg.length());
 						if (stg.equals(p)) {
@@ -473,7 +473,7 @@ public class RepositoryService extends ObjectDTOtransformer implements
 		}
 	}
 
-	private String backlashReplace(String myStr) {
+	private String backslashReplace(String myStr) {
 
 		final StringBuilder result = new StringBuilder();
 		final StringCharacterIterator iterator = new StringCharacterIterator(
@@ -607,22 +607,6 @@ public class RepositoryService extends ObjectDTOtransformer implements
 				folderPath = slashReplace(folderPath);
 				repository.getHiddenFolderPath().remove(folderPath);
 			}
-		}
-	}
-
-	private void determineDestinationPathsForAddonFiles(
-			SyncTreeNode syncTreeNode) {
-
-		if (!syncTreeNode.isLeaf()) {
-			SyncTreeDirectory directory = (SyncTreeDirectory) syncTreeNode;
-			String path = directory.getParent().getDestinationPath();
-			directory.setDestinationPath(new File(path + "/"
-					+ directory.getParent().getName()).getAbsolutePath());
-		} else {
-			SyncTreeLeaf leaf = (SyncTreeLeaf) syncTreeNode;
-			String path = leaf.getParent().getDestinationPath();
-			leaf.setDestinationPath(new File(path + "/"
-					+ leaf.getParent().getName()).getAbsolutePath());
 		}
 	}
 
