2cad082756a63a24bb098206d6f86f30dcbea34c
Major_Shepard <Major_Shepard@73b07b11-db6f-9345-a1c1-cd074a5028ff>
2017-02-28 11:36:16 +0000

    Refactor.
    git-svn-id: svn://www.sonsofexiled.fr/repository/ArmA3Sync/trunk@2527 73b07b11-db6f-9345-a1c1-cd074a5028ff

--- ArmA3Sync/src/fr/soe/a3s/ui/repository/DownloadPanel.java
+++ ArmA3Sync/src/fr/soe/a3s/ui/repository/DownloadPanel.java
@@ -1095,8 +1095,8 @@ public class DownloadPanel extends JPanel implements UIConstants {
 					userconfigUpdater.run();
 				}
 
-				facade.getMainPanel().updateTabs(OP_ADDON_FILES_CHANGED);
 				facade.getAddonsPanel().getGroupManager().updateGroupModsets();
+				facade.getMainPanel().updateTabs(OP_ADDON_FILES_CHANGED);
 
 				// Check for Addons
 				checkForAddons(false);
@@ -1229,9 +1229,9 @@ public class DownloadPanel extends JPanel implements UIConstants {
 					userconfigUpdater.run();
 				}
 
+				facade.getAddonsPanel().getGroupManager().updateGroupModsets();
 				facade.getMainPanel().updateTabs(OP_REPOSITORY_CHANGED);
 				facade.getMainPanel().updateTabs(OP_ADDON_FILES_CHANGED);
-				facade.getAddonsPanel().getGroupManager().updateGroupModsets();
 
 				obs.end();
 			}
